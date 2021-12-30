87504654da3bdd1ef805a3502d26b26b7ccca64c
Adrian Hernandez <danix08@live.com>
2021-12-30 19:28:13 -0300

    agregando y configurando Tailwind

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import TableCoins from './components/TableCoins';
 import './App.css';
 
 function App() {
 
+  const [coins, setCoins] = useState([]);
+
+  const getData = async () => {
+    const res = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+
+    console.log(res.data)
+    setCoins(res.data)
+  }
+
   useEffect(() => {
-
-    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
-
+    getData();
   }, [])
 
 
   return (
-    <div className="App">
-      <h1>Coin Markets</h1>
+    <div className="container">
+      <div className="row">
+        <TableCoins coins={coins} />
+      </div>
     </div>
   );
 }
